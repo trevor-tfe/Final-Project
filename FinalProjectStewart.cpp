1b69d00e577572c9d087491ca96d221557803b33
trevor-tfe <108952876+trevor-tfe@users.noreply.github.com>
2023-04-02 23:06:49 -0500

    Initial commit

---
+++ FinalProjectStewart.cpp
@@ -0,0 +1,39 @@
+/***************************************************************
+* Name : Final Project - Inventory and Scheduling
+* Author: Trevor Stewart
+* Created : 04/01/2023
+* Course: CIS 152 - Data Structure
+* Version: 1.0
+* OS: Windows 10
+* IDE: VS Code and MSYS2
+* Copyright : This is my own original work 
+* based onspecifications issued by our instructor
+* Description : An app that sorts an array in different ways and returns
+* the time it took to use each sort function.
+*   Input: None
+*   Ouput: Display and print delivery receipts
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access
+* to my program.
+***************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Inventory{
+    private:
+    string name;
+    int SKU;
+    double price;
+
+    public:
+};
+
+class Delivery{
+    private:
+    public:
+};
+
+int main(){
+    return 0;
+}
