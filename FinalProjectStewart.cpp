eef3825fde51a828ae65d606f687f3bc114baf8a
trevor-tfe <108952876+trevor-tfe@users.noreply.github.com>
2023-04-09 14:27:56 -0500

    Update FinalProjectStewart.cpp

--- FinalProjectStewart.cpp
+++ FinalProjectStewart.cpp
@@ -11,7 +11,7 @@
 * Description : An app that sorts an array in different ways and returns
 * the time it took to use each sort function.
 *   Input: None
-*   Ouput: Display and print delivery receipts
+*   Ouput: Display and print delivery receipts.
 * Academic Honesty: I attest that this is my original work.
 * I have not used unauthorized source code, either modified or
 * unmodified. I have not given other fellow student(s) access
@@ -19,20 +19,51 @@
 ***************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
+time_t now = time(0);
 
 class Inventory{
     private:
     string name;
     int SKU;
     double price;
-
     public:
+    //CONSTRUCTORS
+    Inventory(){}
+    Inventory(string s){this->name = s;}
+    Inventory(string s, int k){this->name = s; this->SKU = k;}
+    Inventory(string s, int k, double p){this->name = s; this->SKU = k; this->price = p;}
+    //SETTERS
+    void SetName(string s){this->name = s;}
+    void SetSKU(int k){this->SKU = k;}
+    void SetPrice(double p){this->price = p;}
+    //GETTERS
+    string GetName(){return this->name;}
+    int GetSKU(){return this->SKU;}
+    double GetPrice(){return this->price;}
 };
 
 class Delivery{
     private:
+    string contact;
+    string address;
+    string phone;
+    tm *schedule = localtime(&now);
+    list <Inventory> items;
     public:
+    Delivery(){}
+    Delivery(string c){this->contact = c;}
+    Delivery(string c, string a){this->contact = c; this->address = a;}
+    Delivery(string c, string a, string p){this->contact = c; this->address = a; this->phone = p;}
+    tm GetSchedule(){return *schedule;}
 };
+
+bool operator<(Delivery lhs, Delivery rhs){
+    return lhs.GetSchedule().tm_yday < rhs.GetSchedule().tm_yday;
+}
+
+bool operator>(Delivery lhs, Delivery rhs){
+    return lhs.GetSchedule().tm_yday > rhs.GetSchedule().tm_yday;
+}
 
 int main(){
     return 0;
