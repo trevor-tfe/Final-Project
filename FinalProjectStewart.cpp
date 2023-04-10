47949a525aff8626ce4ebc06077ae183aac3633d
trevor-tfe <108952876+trevor-tfe@users.noreply.github.com>
2023-04-09 19:22:26 -0500

    Test info and .gitignore
    Tested time info, need to solidify proper time gathering.

--- FinalProjectStewart.cpp
+++ FinalProjectStewart.cpp
@@ -10,7 +10,7 @@
 * based onspecifications issued by our instructor
 * Description : An app that sorts an array in different ways and returns
 * the time it took to use each sort function.
-*   Input: None
+*   Input: Delivery schedule and dates
 *   Ouput: Display and print delivery receipts.
 * Academic Honesty: I attest that this is my original work.
 * I have not used unauthorized source code, either modified or
@@ -26,20 +26,24 @@ class Inventory{
     string name;
     int SKU;
     double price;
+    int stock;
     public:
     //CONSTRUCTORS
     Inventory(){}
     Inventory(string s){this->name = s;}
     Inventory(string s, int k){this->name = s; this->SKU = k;}
     Inventory(string s, int k, double p){this->name = s; this->SKU = k; this->price = p;}
+    Inventory(string s, int k, double p, int st){this->name = s; this->SKU = k; this->price = p; this->stock = st;}
     //SETTERS
     void SetName(string s){this->name = s;}
     void SetSKU(int k){this->SKU = k;}
     void SetPrice(double p){this->price = p;}
+    void SetStock(int s){this->stock = s;}
     //GETTERS
     string GetName(){return this->name;}
     int GetSKU(){return this->SKU;}
     double GetPrice(){return this->price;}
+    int GetStock(){return this->stock;}
 };
 
 class Delivery{
@@ -50,12 +54,29 @@ class Delivery{
     tm *schedule = localtime(&now);
     list <Inventory> items;
     public:
+    //Constructors
     Delivery(){}
     Delivery(string c){this->contact = c;}
     Delivery(string c, string a){this->contact = c; this->address = a;}
     Delivery(string c, string a, string p){this->contact = c; this->address = a; this->phone = p;}
+    //Setters
+    void SetContact(string c){this->contact = c;}
+    void SetAddress(string a){this->address = a;}
+    void SetPhone(string p){this->phone = p;}
+    void AddItem(Inventory i){this->items.push_back(i);}
+    void SetSchedule(int year, int month, int day){
+        this->schedule->tm_year = year-1900;
+        this->schedule->tm_mon = month-1;
+        this->schedule->tm_mday = day;
+    }
+    //Getters
+    string GetContact(){return contact;}
+    string GetAddress(){return address;}
+    string GetPhone(){return phone;}
+    list<Inventory> GetItems(){return items;}
     tm GetSchedule(){return *schedule;}
 };
+
 
 bool operator<(Delivery lhs, Delivery rhs){
     return lhs.GetSchedule().tm_yday < rhs.GetSchedule().tm_yday;
@@ -66,5 +87,19 @@ bool operator>(Delivery lhs, Delivery rhs){
 }
 
 int main(){
+    list<Inventory> currentStock;
+    list<Delivery> unscheduled;
+    priority_queue<Delivery> docket;
+
+    time_t rawtime;
+    time(&rawtime);
+    struct tm * test;
+    test = localtime(&rawtime);
+    test->tm_mday = 20;
+    test->tm_mon = 2;
+    test->tm_year = 2023;
+    mktime(test);
+
+    cout << test->tm_yday;
     return 0;
 }
